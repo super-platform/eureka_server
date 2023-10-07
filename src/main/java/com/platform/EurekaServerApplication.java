7c8056371eb23fe8c7613bb8f3f0740a6e026874
tutruongmugi <khongthenho304@gmail.com>
2023-10-07 14:40:39 +0700

    config eureka server

---
+++ src/main/java/com/platform/EurekaServerApplication.java
@@ -0,0 +1,73 @@
+package com.platform;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@Slf4j
+@EnableEurekaServer
+public class EurekaServerApplication {
+	private static final String SPRING_PROFILE_DEFAULT = "spring.profiles.default";
+	private static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+	private static final String SPRING_PROFILE_UAT = "uat";
+	private static final String SPRING_PROFILE_PRODUCTION = "prod";
+	private static final String SPRING_PROFILE_CLOUD = "cloud";
+
+	private final Environment env;
+
+	public EurekaServerApplication(Environment env) {
+		this.env = env;
+	}
+
+	@PostConstruct
+	public void initApplication(){
+		Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+		if(activeProfiles.contains(SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(SPRING_PROFILE_PRODUCTION)){
+			log.error("ou have misconfigured your application!");
+		}
+
+		if(activeProfiles.contains(SPRING_PROFILE_UAT) && activeProfiles.contains(SPRING_PROFILE_PRODUCTION)){
+			log.error("ou have misconfigured your application!");
+		}
+
+		if(activeProfiles.contains(SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(SPRING_PROFILE_CLOUD)){
+			log.error("ou have misconfigured your application!");
+		}
+
+	}
+
+	private static void addDefaultProfile(SpringApplication app){
+		Map<String, Object> defProperties = new HashMap<>();
+		defProperties.put(SPRING_PROFILE_DEFAULT, SPRING_PROFILE_DEVELOPMENT);
+		app.setDefaultProperties(defProperties);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(EurekaServerApplication.class);
+
+		addDefaultProfile(app);
+
+		Environment env = app.run(args).getEnvironment();
+		log.info("====================ENV====================: {}", env.getProperty("spring.kafka.bootstrap-servers"));
+
+		String protocol = "http";
+		if(env.getProperty("server.ssl.key-store") != null){
+			protocol = "https";
+		}
+		log.info(
+				"\n---------------------------------------------------------\n\t"
+						+ "Application '{}' is running! Access URLs: \n\t" + "Protocol: \t\t{}\n\t"
+						+ "Profile(s) \t{}\n-----------------------------------------------------------",
+				env.getProperty("spring.application.name"), protocol, env.getActiveProfiles());
+	}
+
+}
